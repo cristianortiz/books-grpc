ec203a624992ee5c8ac243e6d4bc1286913bfed6
cristianortiz <cristian.ortiznavia82@gmail.com>
2025-04-10 22:56:31 -0400

    [ADD] migrations logic  for postgress

--- go.mod
+++ go.mod
@@ -14,7 +14,8 @@ require (
 require (
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
-	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.18.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -28,8 +29,9 @@ require (
 	github.com/spf13/cast v1.7.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
---
+++ internal/pkg/db/migrations/migrator.go
@@ -0,0 +1,91 @@
+package migrations
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"path/filepath"
+	"strings"
+
+	"github.com/cristianortiz/books-grpc/internal/pkg/configs"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	cutSet       = "file://" //represents the prefix to cut from migration directory path
+	databaseName = "postgres"
+)
+
+type Migrator struct {
+	pgDBMigrate *migrate.Migrate
+}
+
+// provides a migrator instance, it takes a db config and gorm dbConn as input. It initializes and returns
+// a migrator instance with the postgres db migrator
+func ProvideMigrator(config configs.DatabaseConfig, pgDB *gorm.DB) (*Migrator, error) {
+	dbConn, err := pgDB.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	pgDBMigrate, err := initMigrate(dbConn, config.MigrationPath)
+	if err != nil {
+		return nil, err
+	}
+	return &Migrator{
+		pgDBMigrate: pgDBMigrate,
+	}, nil
+}
+
+func (m Migrator) RunMigrations() {
+	m.RunMigrationsWith(m.pgDBMigrate, "Postgres Database")
+}
+func (m Migrator) RunMigrationsWith(migrateInstance *migrate.Migrate, dbName string) {
+	if err := migrateInstance.Up(); err != nil {
+		if err == migrate.ErrNoChange {
+			log.Printf("no change detected after running the migrations for %s", dbName)
+			return
+		}
+		log.Println(fmt.Sprintf("migration Failed for %s", dbName), err)
+	}
+	log.Printf("migrations applied succesfully to  %s", dbName)
+
+}
+
+// initializes the migration instance for postgres db. it takes sql.DB connection and a migrations
+// directory as input. It creates a postgres driver instance and then initializes the migration inst
+// using db driver
+func initMigrate(dbConn *sql.DB, dir string) (*migrate.Migrate, error) {
+	//creates postgres driver instance
+	driver, err := postgres.WithInstance(dbConn, &postgres.Config{})
+	if err != nil {
+		return nil, err
+	}
+	sourcePath, err := getSourcePath(dir)
+	if err != nil {
+		return nil, err
+	}
+	//initializes migrations instance usgin db driver
+	m, err := migrate.NewWithDatabaseInstance(sourcePath, databaseName, driver)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+// process the migration directory path. It trims the "file://" prefix, obtains the absolute path,
+// and returns the formattes source path required for migration
+func getSourcePath(dir string) (string, error) {
+	dir = strings.TrimPrefix(dir, cutSet)
+	// absolute path is important to avoid error finding the migrations directory route, ex, if the route is defined as relative
+	// or outside the project directory or in other level, ex input "migrations" (relative ) and app executes
+	//  from /app, filepath.abs returns "/app/migrations" (absolute)
+	absPath, err := filepath.Abs(dir)
+	if err != nil {
+		return "", err
+	}
+	//now add again prefix required for migrate pkg to the abs path: returns "file:///app/migrations"
+	return fmt.Sprintf("%s%s", cutSet, absPath), nil
+}
